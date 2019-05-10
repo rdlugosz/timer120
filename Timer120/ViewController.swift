125d130bd72cf0e16686f1396d122c7cb0a40514
Ryan Dlugosz <ryan@dlugosz.net>
2019-05-10 10:27:32 -0400

    Initial Commit

---
+++ Timer120/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Timer120
+//
+//  Created by Ryan Dlugosz on 5/10/19.
+//  Copyright © 2019 Back Cove Software, LLC. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
